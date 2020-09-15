e6b4b2d0213c35df47f870ee5fbb96cbdc9d0a6a
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2020-09-15 23:32:50 +0400

    test: flip biased coin

--- src/coin-fns.spec.ts
+++ src/coin-fns.spec.ts
@@ -6,13 +6,13 @@ type Pair<T> = [T, T];
 
 type PickWithRandom<T> = (random: number) => T;
 
-function flipFairCoin<T>(options: Pair<T>, random: number): T; // full
+function flipFairCoin<T>(options: Pair<T>, random: number): T; // total
 function flipFairCoin<T>(options: Pair<T>): PickWithRandom<T>; // curried
 
 function flipFairCoin<T>(options: Pair<T>, random?: number) {
   const fairRatio = 0.5;
 
-  const pickWithRandom = (random: number) =>
+  const pickWithRandom: PickWithRandom<T> = (random: number): T =>
     random > fairRatio ? options[0] : options[1];
 
   return random ? pickWithRandom(random) : pickWithRandom;
@@ -24,9 +24,45 @@ Deno.test("flipFairCoin", () => {
   const assertValidOption = (expected: number, msg?: string) =>
     assertArrayContains(options, [expected], msg);
 
-  assertValidOption(flipFairCoin(options, Math.random()), "total args");
+  assertValidOption(flipFairCoin(options, Math.random()), "total");
 
   const flipWith = flipFairCoin(options);
+  assertValidOption(flipWith(Math.random()), "curried");
+  assertValidOption(flipWith(Math.random()), "curried");
+});
+
+function flipBiasedCoin<T>(
+  ratio: number,
+  options: Pair<T>,
+  random: number,
+): T;
+
+function flipBiasedCoin<T>(
+  ratio: number,
+  options: Pair<T>,
+): PickWithRandom<T>;
+
+function flipBiasedCoin<T>(
+  ratio: number,
+  options: Pair<T>,
+  random?: number,
+) {
+  const pickWithRandom: PickWithRandom<T> = (random: number): T =>
+    random > ratio ? options[0] : options[1];
+
+  return random ? pickWithRandom(random) : pickWithRandom;
+}
+
+Deno.test("flipBiasedCoin", () => {
+  const options = [1, 2] as Pair<number>;
+  const ratio = 0.33;
+
+  const assertValidOption = (expected: number, msg?: string) =>
+    assertArrayContains(options, [expected], msg);
+
+  assertValidOption(flipBiasedCoin(ratio, options, Math.random()), "total");
+
+  const flipWith = flipBiasedCoin(ratio, options);
   assertValidOption(flipWith(Math.random()), "curried");
   assertValidOption(flipWith(Math.random()), "curried");
 });
