3cc77ad724332415b9fc73fcf23c2968bd35b88f
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2020-09-15 23:42:27 +0400

    test: drip `flipFairCoin` implementation
    The `flipFairCoin` is a special case for `flipBiasedCoin`,
    when `ratio = 0.5`

--- src/coin-fns.spec.ts
+++ src/coin-fns.spec.ts
@@ -6,31 +6,6 @@ type Pair<T> = [T, T];
 
 type PickWithRandom<T> = (random: number) => T;
 
-function flipFairCoin<T>(options: Pair<T>, random: number): T; // total
-function flipFairCoin<T>(options: Pair<T>): PickWithRandom<T>; // curried
-
-function flipFairCoin<T>(options: Pair<T>, random?: number) {
-  const fairRatio = 0.5;
-
-  const pickWithRandom: PickWithRandom<T> = (random: number): T =>
-    random > fairRatio ? options[0] : options[1];
-
-  return random ? pickWithRandom(random) : pickWithRandom;
-}
-
-Deno.test("flipFairCoin", () => {
-  const options = [1, 2] as Pair<number>;
-
-  const assertValidOption = (expected: number, msg?: string) =>
-    assertArrayContains(options, [expected], msg);
-
-  assertValidOption(flipFairCoin(options, Math.random()), "total");
-
-  const flipWith = flipFairCoin(options);
-  assertValidOption(flipWith(Math.random()), "curried");
-  assertValidOption(flipWith(Math.random()), "curried");
-});
-
 function flipBiasedCoin<T>(
   ratio: number,
   options: Pair<T>,
@@ -65,4 +40,18 @@ Deno.test("flipBiasedCoin", () => {
   const flipWith = flipBiasedCoin(ratio, options);
   assertValidOption(flipWith(Math.random()), "curried");
   assertValidOption(flipWith(Math.random()), "curried");
+});
+
+Deno.test("flipFairCoin", () => {
+  const options = [1, 2] as Pair<number>;
+  const fairRatio = 0.5;
+
+  type FlipFairCoin<T> = (options: Pair<T>, random: number) => T;
+  const flipFairCoin: FlipFairCoin<number> = (options, random) =>
+    flipBiasedCoin(fairRatio, options, random);
+
+  const assertValidOption = (expected: number, msg?: string) =>
+    assertArrayContains(options, [expected], msg);
+
+  assertValidOption(flipFairCoin(options, Math.random()), "total");
 });
