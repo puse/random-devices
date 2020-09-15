12aa5861fafc2876bead61ae8a10596b15fdd86e
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2020-09-16 01:47:09 +0400

    test: flip results distribution

--- src/coin-fns.spec.ts
+++ src/coin-fns.spec.ts
@@ -1,4 +1,5 @@
 import {
+  assert,
   assertArrayContains,
 } from "https://deno.land/x/std@0.65.0/testing/asserts.ts";
 
@@ -13,4 +14,20 @@ Deno.test("flipBiasedCoin - signature", () => {
     assertArrayContains(options, [expected], msg);
 
   assertValidOption(flipBiasedCoin(ratio, options, Math.random()));
+});
+
+Deno.test("flipBiasedCoin - distribution", () => {
+  const flip = () => flipBiasedCoin(0.66, ["a", "b"], Math.random());
+  const results = Array.from(new Array(100), flip)
+
+  const countDistinct = (acc: Record<string, number>, option: string) => {
+    acc[option]++;
+    return acc;
+  };
+  const stats = results.reduce(countDistinct, { a: 0, b: 0});
+
+  assert(stats.a < stats.b)
+
+  console.log('\n')
+  console.table(stats)
 });
