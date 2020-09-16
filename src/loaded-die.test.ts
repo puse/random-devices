1329d62536bf95c8c05ecffc85f4dfcd5cc5875e
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2020-09-16 21:06:21 +0400

    test: roll die with aliasing index

--- src/loaded-die.test.ts
+++ src/loaded-die.test.ts
@@ -5,54 +5,69 @@ import {
 import { createBiasedCoin, flipBiasedCoin } from "./biased-coin.ts";
 import { RandomNumberGenerator } from "./common-types.ts";
 
-type WeightedOptions<T> = Map<T, number>;
+const makeTable = (options: [unknown, number][]) => {
+  const card = options.length
 
-type WeightedOptionEntry<T> = [T, number];
-type WeightedOption<T> = {
-  value: T;
-  weight: number;
+  const totalWeight = options
+    .map(([_, weight]) => weight)
+    .reduce((a, b) => a + b);
+  const meanWeight = totalWeight / card;
+
+  const acc = [];
+
+  while (acc.length < card) {
+    const probeIdx = options.findIndex(([_, weight]) => weight < meanWeight);
+
+    // only exact left
+    if (probeIdx === -1) {
+      return [...acc, ...options.map((x) => [x])];
+    }
+
+    const [probe] = options.splice(probeIdx, 1);
+
+    const deltaWeight = meanWeight - probe[1];
+
+    const donorIdx = options.findIndex(([_, weight]) => weight > meanWeight);
+    const [donor] = options.splice(donorIdx, 1);
+
+    const alias = [donor[0], deltaWeight] as [unknown, number];
+    const leftover = [donor[0], donor[1] - deltaWeight] as [unknown, number];
+
+    acc.push([probe, alias]);
+    options.push(leftover);
+  }
+
+  return acc;
 };
 
-const toWeightedOption = <T>(
-  entry: WeightedOptionEntry<T>,
-): WeightedOption<T> => {
-  const [value, weight] = entry;
-
-  return {
-    value,
-    weight,
-  };
-};
-
-function rollLoadedDie<T>(
+function rollLoadedDie(
   rng: RandomNumberGenerator,
-  options: WeightedOptions<T>,
-): T {
-  const entries = Array
-    .from(options.entries())
-    .map(toWeightedOption);
+  options: Map<unknown, number>
+): unknown {
+  const entries = Array.from(options.entries())
 
-  const totalWeight = Array
-    .from(options.values())
-    .reduce((a, b) => a + b, 0);
+  const card = entries.length
 
-  const pickEventually = (nthAttempt: number = 0): T => {
-    const entry = entries[nthAttempt % options.size];
-    const coin = createBiasedCoin(entry.weight / totalWeight, [true, false]);
-    return flipBiasedCoin(rng, coin)
-      ? entry.value
-      : pickEventually(nthAttempt + 1);
-  };
+  if (card === 1) {
+    const [value] = entries[0]
+    return value
+  }
 
-  return pickEventually();
+  if (card === 2) {
+    const [[v1, w1], [v2, w2]] = entries
+    return rng() < w1 / (w1 + w2) ? v1 : v2
+  }
+
+  const balancedSlots = makeTable(entries)
+  const slotIdx = Math.floor(rng() * card)
+
+  return rollLoadedDie(rng, new Map(balancedSlots[slotIdx]));
 }
 
 Deno.test("rollLoadedDie - signature", () => {
   const options = new Map<string, number>([
     ["a", 1],
     ["b", 2],
-    ["c", 3],
-    ["d", 6],
   ]);
   const result = rollLoadedDie(Math.random, options);
 
@@ -70,7 +85,7 @@ Deno.test("rollLoadedDie - distribution", () => {
   const results = Array.from(new Array(1000), rollOnce);
 
   // aggregate stats
-  const countDistinct = (acc: Record<string, number>, result: string) => {
+  const countDistinct = (acc: Record<string, number>, result: any) => {
     if (acc[result] === undefined) {
       acc[result] = 0;
     }
@@ -83,4 +98,22 @@ Deno.test("rollLoadedDie - distribution", () => {
   console.table(stats);
 
   assert(true);
+});
+
+Deno.test("alias table", () => {
+  const options = new Map<string, number>([
+    ["a", 1],
+    ["b", 2],
+    ["c", 3],
+    ["d", 6],
+  ]);
+
+  const table = [
+    [["a", 1], ["d", 2]],
+    [["b", 2], ["d", 1]],
+    [["c", 3]],
+    [["d", 3]],
+  ];
+
+  console.log(makeTable(Array.from(options.entries())))
 });
