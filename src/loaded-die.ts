ea2c852863c9ccd26a6d9bf56c0c9d910e07352d
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2020-09-16 22:28:30 +0400

    fix: improve loaded die

--- src/loaded-die.ts
+++ src/loaded-die.ts
@@ -1,60 +1,84 @@
 import { RandomNumberGenerator } from "./common-types.ts";
 
-export const makeTable = (options: [unknown, number][]) => {
-  const card = options.length;
+type Pred = (...args: any[]) => boolean;
 
+const sliceOneBy = <T>(pred: Pred, arrRef: T[]): [T | null, T[]] => {
+  // mutate copy
+  const arr = [...arrRef];
+  const idx = arr.findIndex(pred);
+  if (idx === -1) {
+    return [null, arr];
+  } else {
+    const [el] = arr.splice(idx, 1);
+    return [el, arr];
+  }
+};
+
+export type WeightedOption<T> = [T, number];
+
+function popOverWeight<T>(
+  offset: number,
+  list: WeightedOption<T>[],
+): [WeightedOption<T> | null, WeightedOption<T>[]] {
+  return sliceOneBy(([_, weight]) => weight > offset, list);
+}
+
+function popUnderWeight<T>(
+  offset: number,
+  list: WeightedOption<T>[],
+): [WeightedOption<T> | null, WeightedOption<T>[]] {
+  return sliceOneBy(([_, weight]) => weight < offset, list);
+}
+
+export const nthBalancedOption = <T>(
+  nth: number,
+  options: WeightedOption<T>[],
+): WeightedOption<T>[] => {
   const totalWeight = options
     .map(([_, weight]) => weight)
     .reduce((a, b) => a + b);
-  const meanWeight = totalWeight / card;
+  const meanWeight = totalWeight / options.length;
 
-  const acc = [];
+  // pick an underweight
+  const [probe, restOptions] = popUnderWeight(meanWeight, options);
 
-  while (acc.length < card) {
-    const probeIdx = options.findIndex(([_, weight]) => weight < meanWeight);
-
-    // only exact left
-    if (probeIdx === -1) {
-      return [...acc, ...options.map((x) => [x])];
-    }
-
-    const [probe] = options.splice(probeIdx, 1);
-
-    const deltaWeight = meanWeight - probe[1];
-
-    const donorIdx = options.findIndex(([_, weight]) => weight > meanWeight);
-    const [donor] = options.splice(donorIdx, 1);
-
-    const alias = [donor[0], deltaWeight] as [unknown, number];
-    const leftover = [donor[0], donor[1] - deltaWeight] as [unknown, number];
-
-    acc.push([probe, alias]);
-    options.push(leftover);
+  if (!probe) {
+    return [options[nth]];
   }
 
-  return acc;
+  const [donor, restOptions2] = popOverWeight(meanWeight, restOptions);
+
+  if (!donor) {
+    throw RangeError("Can not find donor");
+  }
+
+  const deltaWeight = meanWeight - probe[1];
+
+  if (nth === 0) {
+    const alias = [donor[0], deltaWeight] as WeightedOption<T>;
+    return [probe, alias];
+  } else {
+    const leftover = [donor[0], donor[1] - deltaWeight] as WeightedOption<T>;
+    return nthBalancedOption(nth - 1, [...restOptions2, leftover]);
+  }
 };
 
-export function rollLoadedDie(
+export function rollLoadedDie<T>(
   rng: RandomNumberGenerator,
-  options: Map<unknown, number>,
-): unknown {
-  const entries = Array.from(options.entries());
-
-  const card = entries.length;
-
-  if (card === 1) {
-    const [value] = entries[0];
-    return value;
+  options: WeightedOption<T>[],
+): T {
+  switch (options.length) {
+    case 1:
+      const [value] = options[0];
+      return value;
+    case 2:
+      const [[v1, w1], [v2, w2]] = options;
+      return rng() < w1 / (w1 + w2) ? v1 : v2;
+    default:
+      const slot = nthBalancedOption(
+        Math.floor(rng() * options.length),
+        options,
+      );
+      return rollLoadedDie(rng, slot);
   }
-
-  if (card === 2) {
-    const [[v1, w1], [v2, w2]] = entries;
-    return rng() < w1 / (w1 + w2) ? v1 : v2;
-  }
-
-  const balancedSlots = makeTable(entries);
-  const slotIdx = Math.floor(rng() * card);
-
-  return rollLoadedDie(rng, new Map(balancedSlots[slotIdx]));
 }
