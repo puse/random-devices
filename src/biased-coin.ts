1baae5964f8245ff68377b2995a35778388ea6b5
mushegha <25614707+mushegha@users.noreply.github.com>
2020-09-16 13:21:43 +0400

    fix: separate create* and flip* for biased coin (#3)
    * implement `createBiasedCoin`
    * update flip* to accept coin

--- src/biased-coin.ts
+++ src/biased-coin.ts
@@ -1,22 +1,33 @@
-import type { Pair, RandomNumberGenerator, CoinSide } from "./common-types.ts";
+import type { Pair, RandomNumberGenerator } from "./common-types.ts";
 
-/** Pick a side for given index based on ratio */
-const sideFor = (ratio: number, index: number): CoinSide => {
-  const offset = 1 / (ratio + 1);
-  return offset < index ? 0 : 1;
+export type BiasedCoin<T> = {
+  ratio: number;
+  sides: Pair<T>;
 };
 
+export function createBiasedCoin<T>(
+  ratio: number,
+  sides: Pair<T>,
+): BiasedCoin<T> {
+  return {
+    ratio,
+    sides,
+  };
+}
+
 /**
- * Pick one *randomly* from given options pair.
+ * Pick one *randomly* from given biased coin.
  * Choice of option is biased by `ratio`
  *
- *    flipBiasedCoin(Math.random, 2/3, ['a', 'b']);
+ *    const coin = createBiasedCoin(1 / 3, ['a', 'b']);
+ *    flipBiasedCoin(Math.random, coin);
  */
 
 export function flipBiasedCoin<T>(
   rng: RandomNumberGenerator,
-  ratio: number,
-  options: Pair<T>,
-) {
-  return options[sideFor(ratio, rng())];
+  coin: BiasedCoin<T>,
+): T {
+  const headRange = coin.ratio / (coin.ratio + 1);
+  const [head, tail] = coin.sides;
+  return rng() < headRange ? head : tail;
 }
