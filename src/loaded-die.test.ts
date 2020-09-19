8578d9cd2e5e8e2674201ed0ff6a62f705ecc28f
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2020-09-19 16:56:28 +0400

    test: simplify loaded die internals

--- src/loaded-die.test.ts
+++ src/loaded-die.test.ts
@@ -3,7 +3,57 @@ import {
   assertArrayContains,
 } from "https://deno.land/x/std@0.65.0/testing/asserts.ts";
 
-import { rollLoadedDie } from "./loaded-die.ts";
+import type { NonEmptyList, RNG } from "./common-utils.ts";
+
+//
+
+// --
+
+function randomLoaded(rng: RNG, load: NonEmptyList<number>): number {
+  const [ratios, aliases] = ratiosWithAliases(load)
+
+  const slotIdx = Math.floor(rng() * load.length);
+  return rng() < ratios[slotIdx] ? slotIdx : aliases[slotIdx]
+}
+
+function ratiosWithAliases(weights: number[]) {
+  const mean = weights.reduce((a, b) => a + b) / weights.length;
+  const load = weights.map((x) => x / mean);
+
+  const boundedLoad = [];
+  const aliases = [];
+
+  const visited = Array.from(load, () => false)
+
+  for (let i = 0; i < load.length; i++) {
+    const lteIdx = load.findIndex((x, i) => x <= 1 && !visited[i]);
+    const gteIdx = load.findIndex((x, i) => x >= 1 && !visited[i]);
+
+    visited[lteIdx] = true
+    boundedLoad[lteIdx] = load[lteIdx];
+    aliases[lteIdx] = gteIdx;
+
+    if (lteIdx !== gteIdx) {
+      load[gteIdx] -= 1 - load[lteIdx];
+    }
+  }
+
+  return [
+    boundedLoad,
+    aliases,
+  ];
+}
+
+// --
+
+function rollLoadedDie<A>(rng: RNG, weightedOptions: Map<A, number>): A {
+  const options = [...weightedOptions.keys()]
+  const weights = [...weightedOptions.values()] as NonEmptyList<number>
+
+  return options[randomLoaded(rng, weights)];
+}
+
+// --
 
 Deno.test("rollLoadedDie - signature", () => {
   const options = new Map<string, number>([
@@ -12,10 +62,8 @@ Deno.test("rollLoadedDie - signature", () => {
     ["c", 3],
     ["d", 4],
   ]);
-  const entries = Array.from(options.entries());
-  console.table(entries);
 
-  const result = rollLoadedDie(Math.random, entries);
+  const result = rollLoadedDie(Math.random, options);
 
   console.log(">", result);
 
@@ -29,8 +77,7 @@ Deno.test("rollLoadedDie - distribution", () => {
     ["c", 3],
     ["d", 6],
   ]);
-  const entries = Array.from(options.entries());
-  const rollOnce = () => rollLoadedDie(Math.random, entries);
+  const rollOnce = () => rollLoadedDie(Math.random, options);
   const results = Array.from(new Array(1000), rollOnce);
 
   // aggregate stats
